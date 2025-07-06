72748d032d20cb45ed922923456943ea3b3ee172
sravanpant <sravan.pant5@gmail.com>
2025-07-06 13:17:08 +0530

    test: add Jest config for unit testing

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+};
